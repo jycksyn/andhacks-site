7484c6dd465147d8ddd6bffdcece2f46c6f08445
jackson <jackson.kubin@gmail.com>
2024-07-07 19:05:54 -0400

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,7 @@ export default defineConfig({
   integrations: [tailwind(), icon(), mdx()],
   image: {
     domains: ["i.imgur.com"]
-  }
+  },
+  output: "server",
+  adapter: netlify()
 });
